8c1819016edd24ab8161289a07871f830e45f546
alexleethinker <alexleethinker@gmail.com>
2018-03-28 18:03:14 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ function install_PACKAGES {
   sudo apt install -y xorg
   sudo apt install -y sakura
   sudo apt install -y network-manager-gnome volumeicon-alsa pulseaudio pavucontrol fonts-arphic-uming
-  sudo apt install -y fcitx-googlepinyin
+  #sudo apt install -y fcitx-googlepinyin
   sudo add-apt-repository 'deb https://deb.opera.com/opera-stable/ stable non-free'
   wget -qO- https://deb.opera.com/archive.key | sudo apt-key add -
   sudo apt update
