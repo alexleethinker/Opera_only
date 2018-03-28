2428059f59c7fc65c689c6f5a124f3d1f9a827dc
alexleethinker <alexleethinker@gmail.com>
2018-03-28 18:01:50 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,10 +23,12 @@ function install_PACKAGES {
 function configure {
   
   echo -e "exec --no-startup-id opera" >> ~/.config/i3/config 
+  wget http://cdn2.ime.sogou.com/dl/index/1509619794/sogoupinyin_2.2.0.0102_amd64.deb?st=2x-8JcNU30u_1c669mX_jQ&e=1522245428&fn=sogoupinyin_2.2.0.0102_amd64.deb
 }
 
 function main {
   install_PACKAGES
+  configure
 }
 
 main
