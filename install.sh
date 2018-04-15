4a2d8196d4e926c768a124d4d84e79a0217de190
alexleethinker <alexleethinker@gmail.com>
2018-04-15 16:50:04 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,19 +11,22 @@ function install_PACKAGES {
   sudo apt install -y sakura
   sudo apt install -y network-manager-gnome volumeicon-alsa pulseaudio pavucontrol fonts-arphic-uming
   #sudo apt install -y fcitx-googlepinyin
+  sudo add-apt-repository 'deb http://httpredir.debian.org/debian/ stretch main contrib non-free'
   sudo add-apt-repository 'deb https://deb.opera.com/opera-stable/ stable non-free'
   wget -qO- https://deb.opera.com/archive.key | sudo apt-key add -
   sudo apt update
+  sudo apt-get install firmware-iwlwifi
+  sudo modprobe -r iwlwifi ; modprobe iwlwifi
   sudo apt -y install opera-stable
-  sudo apt -y install i3
-  sudo apt -y install sddm
+  sudo apt -y install openbox obmenu
+  #sudo apt -y install sddm
 
 }
 
 function configure {
   
-  echo -e "exec --no-startup-id opera" >> ~/.config/i3/config 
-  wget http://cdn2.ime.sogou.com/dl/index/1509619794/sogoupinyin_2.2.0.0102_amd64.deb?st=2x-8JcNU30u_1c669mX_jQ&e=1522245428&fn=sogoupinyin_2.2.0.0102_amd64.deb
+  echo -e "exec --no-startup-id opera-stable" >> ~/.config/i3/config 
+  #wget http://cdn2.ime.sogou.com/dl/index/1509619794/sogoupinyin_2.2.0.0102_amd64.deb?st=2x-8JcNU30u_1c669mX_jQ&e=1522245428&fn=sogoupinyin_2.2.0.0102_amd64.deb
 }
 
 function main {
