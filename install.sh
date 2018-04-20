f3c4a99103baf10f7e4c60df29820a0d025aa008
alexleethinker <alexleethinker@gmail.com>
2018-04-20 15:50:39 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,9 +11,9 @@ function install_PACKAGES {
   apt install -y software-properties-common fcitx ttf-wqy-zenhei
   apt install -y xorg
   apt install -y sakura
-  apt install -y xfce4-power-manager dirmngr 
+  apt install -y xfce4-power-manager
   apt install -y apt-transport-https
-  apt install -y wicd volumeicon-alsa pulseaudio pavucontrol fonts-arphic-uming
+  apt install -y volumeicon-alsa pulseaudio pavucontrol fonts-arphic-uming
   apt install -y fcitx-googlepinyin
   add-apt-repository 'deb http://httpredir.debian.org/debian/ stretch main contrib non-free'
   add-apt-repository 'deb http://ftp.de.debian.org/debian sid main contrib' #flash-plugin
@@ -28,18 +28,13 @@ function install_PACKAGES {
   apt -y install slim
   
   apt autoremove && apt clean
+  echo ""
 
 }
 
-function configure {
-  
-  echo -e "exec --no-startup-id opera-stable" >> ~/.config/i3/config 
-  #wget http://cdn2.ime.sogou.com/dl/index/1509619794/sogoupinyin_2.2.0.0102_amd64.deb?st=2x-8JcNU30u_1c669mX_jQ&e=1522245428&fn=sogoupinyin_2.2.0.0102_amd64.deb
-}
 
 function main {
   install_PACKAGES
-  #configure
 }
 
 main
